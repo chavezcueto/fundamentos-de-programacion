86c6ad4bcc6272f50edb106294da90251bd7ea03
Scott1912 <scott1912c@gmail.com>
2024-02-19 17:56:29 -0600

    19_02_24

---
+++ bienvenido.cpp
@@ -0,0 +1,9 @@
+/*programa de bienvenida*/
+/*alumno: Jaretzy Yajhira Chavez Cueto*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "¡Bienvenido" << endl << "  a  " << endl << "c++! " << endl;
+	return 0;
+}
